3ff59dcd922357e197135cd0a6b7b98307057ac7
smilu97 <smilup2244@gmail.com>
2020-07-07 22:35:56 +0900

    modify apis.contests

--- lib/apis/contests.dart
+++ lib/apis/contests.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class ContestListItem {
+class ContestListResponse {
   final String createdAt;
   final String updatedAt;
   final String id;
   final String title;
   final int    numItems;
 
-  ContestListItem({
+  ContestListResponse({
     this.createdAt,
     this.updatedAt,
     this.id,
@@ -16,8 +16,8 @@ class ContestListItem {
     this.numItems,
   });
 
-  factory ContestListItem.fromJson(Map<String, dynamic> json) {
-    return ContestListItem(
+  factory ContestListResponse.fromJson(Map<String, dynamic> json) {
+    return ContestListResponse(
       createdAt: json['created_at'],
       updatedAt: json['updated_at'],
       id:        json['id'],
@@ -27,24 +27,12 @@ class ContestListItem {
   }
 }
 
-class ContestListResponse {
-  final List<ContestListItem> items;
-
-  ContestListResponse({this.items});
-
-  factory ContestListResponse.fromJson(List<dynamic> json) {
-    return ContestListResponse(
-      items: json.map((e) {
-        return ContestListItem.fromJson(e);
-      }).toList()
-    );
-  }
-}
-
-Future<ContestListResponse> fetchContests() async {
+Future<List<ContestListResponse>> fetchContests() async {
   final response = await http.get('http://api.yjin.kim/contests');
   if (response.statusCode == 200) {
-    return ContestListResponse.fromJson(jsonDecode(response.body));
+    return (jsonDecode(response.body) as List<dynamic>).map((e) {
+      return ContestListResponse.fromJson(e);
+    }).toList();
   } else {
     throw Exception('Failed to fetch contests');
   }
